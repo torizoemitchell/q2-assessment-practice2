5e9eefecc691e58f7e408e49866b662fcb048b6c
Tori Mitchell <tori.zoe.mitchell@gmail.com>
2018-10-24 21:03:45 -0600

    database and express setup

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,12 @@
-// Update with your config settings.
-
+// Define DB connections for different environments
 module.exports = {
-
-  development: { },
-  test: { },
-  production: { },
-
-};
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/messages'
+  },
+  test: {},
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
+}
